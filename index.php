0847716053fb26bb19e0f7a3e5b8ab4eac9460e4
Pascal TOVOHERY <tovoherypascal@gmail.com>
2020-08-22 01:07:10 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+  $
+?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <title>Libraries</title>
+</head>
+<body>
+  <h3>Liste libraries</h3>
+  <?php foreach ($variable as $key => $value): ?>
+
+  <?php endforeach; ?>
+</body>
+</html>
